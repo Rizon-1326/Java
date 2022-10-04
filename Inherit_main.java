acdce35bd953a89770f8d9bf5512b03dc849407e
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-10-04 21:14:55 +0600

    Create Inherit_main.java

---
+++ Inherit_main.java
@@ -0,0 +1,16 @@
+package inherit;
+
+public class Inherit_main {
+public static void main(String[] args) {
+	
+	student Std=new student();
+	Std.name="Rizon";
+	Std.age=19;
+	
+	student.roll=1326;
+	student.address="Mymensingh";
+	
+	Std.display1();
+	
+}
+}
