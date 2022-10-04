6531828452aa938373d3e012b4ec9564edcdbb24
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-10-04 19:47:12 +0600

    Create Encap_1.java

---
+++ Encap_1.java
@@ -0,0 +1,22 @@
+package encapsulation;
+
+public class Encap {
+private String name;
+private int age;
+
+public void setN(String name)
+{
+	this.name=name;
+}
+public String getN() {
+	return name;
+}
+
+public void setAge(int age)
+{
+	this.age=age;
+}
+public int getAge() {
+	return age;
+}
+}
