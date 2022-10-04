670727f1def2299aafe261d3d6827bada91b2890
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-10-04 19:49:48 +0600

    Create Static.java

---
+++ Static.java
@@ -0,0 +1,12 @@
+package rizon;
+
+public class Static_method {
+static int roll;
+static String name;
+
+static void display_info()
+{
+	System.out.println("Roll:"+roll);
+	System.out.println("Name:"+name);
+}
+}
