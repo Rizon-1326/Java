313349b14743bf2dc1d5af0a9b01aa442efa056e
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-10-04 19:47:57 +0600

    Create Encap_main.java

---
+++ Encap_main.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class Encap_main {
+public static void main(String[] args) {
+	Encap p1=new Encap();
+	p1.setN("Rizon");
+	System.out.println(p1.getN());
+	
+	p1.setAge(21);
+	System.out.println(p1.getAge());
+}
+}
