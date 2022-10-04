dd2168a66fd479ab9491c780c80e6ed06e9a4dfa
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-10-04 21:13:48 +0600

    Create Inherit_person.java

---
+++ Inherit_person.java
@@ -0,0 +1,12 @@
+package inherit;
+
+public class Person {
+	String name;
+	int age;
+	void display()
+	{
+		System.out.println("Person info");
+		System.out.println("Name"+name);
+		System.out.println("Age"+age);
+	}
+}
