c6562c218223b9622ea3b8a194b33cd1b4453ff6
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-10-04 19:49:16 +0600

    Create Box_problemmain.java

---
+++ Box_problemmain.java
@@ -0,0 +1,12 @@
+package problem;
+
+public class Box_main {
+public static void main(String[] args) {
+	Box data=new Box(10,10,10);
+	data.display_vol();
+
+	Box data1=new Box(20,20,20);
+
+	data1.display_vol();
+}
+}
