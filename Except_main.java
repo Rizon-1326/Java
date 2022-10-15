59e80d68e5a3bf6fcd59974909254534b05252da
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-10-16 00:03:32 +0600

    Create Except_main.java

---
+++ Except_main.java
@@ -0,0 +1,14 @@
+package exception_handling;
+
+
+     
+	public class rizon extends Exception  
+	{  
+	    public rizon(String str)  
+	    {  
+	        // Calling constructor of parent Exception  
+	        super(str);  
+	    }  
+	}  
+        
+
